8461f4e3c878c8f8f58a5762a82ba3311733e2d5
nashira1 <107013826+nashira1@users.noreply.github.com>
2023-10-10 20:05:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function tocaSomPOm(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeBotoes = document.querySelectorAll(".tecla")
+
+listaDeBotoes[0].onclick = tocaSomPom;
